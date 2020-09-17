5d805dd6611bf1f200073d8168ff96884d2f9a71
Samir OMAR <samosyzigii1@gmail.com>
2020-09-17 15:54:16 +0300

    issue 1: main module function get_next_letter

--- main.py
+++ main.py
@@ -46,17 +46,17 @@ def get_next_letter(remaining_letters):
     if len(remaining_letters) == 0:
         raise ValueError('There are no remaining letters')
     while True:
-        next_letter = input('Choose the next letter: ')
+        next_letter = str(input('Choose the next letter: '))
         next_letter.lower()
         if len(next_letter) != 1:
             print('{0} is not a single character'.format(next_letter))
-        elif next_letter not in ascii_lowercase:
+        elif next_letter not in str(ascii_lowercase):
             print('{0} is not a letter'.format(next_letter))
         elif next_letter not in remaining_letters:
             print('{0} has been guessed before'.format(next_letter))
         else:
             remaining_letters.remove(next_letter)
-        return next_letter
+            return next_letter
 
 
 def play_hangman():
